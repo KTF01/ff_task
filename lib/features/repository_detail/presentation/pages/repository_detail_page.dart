6bb534aa9bd0b23d27d7d89a93c0c03d5280e5a1
kristof <kristof.horvath@exagent.group>
2025-12-09 22:06:41 +0100

    detail page

---
+++ lib/features/repository_detail/presentation/pages/repository_detail_page.dart
@@ -0,0 +1,41 @@
+import 'package:ff_task/features/repository_detail/presentation/widgets/description_area.dart';
+import 'package:ff_task/features/repository_detail/presentation/widgets/info_row.dart';
+import 'package:ff_task/features/repository_detail/presentation/widgets/link_button.dart';
+import 'package:ff_task/features/repository_detail/presentation/widgets/profile_header.dart';
+import 'package:ff_task/features/repository_list/data/models/repository_model.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class RepositoryDetailPage extends StatelessWidget {
+  static const String routeName = 'detail';
+
+  const RepositoryDetailPage(this.repository, {super.key});
+
+  final Object repository;
+
+  @override
+  Widget build(BuildContext context) {
+    final RepositoryModel repo = repository as RepositoryModel;
+    final Uri repoUrl = Uri.parse(repo.htmlUrl);
+    final format = DateFormat('yyyy-MM-dd hh:mm');
+    return Scaffold(
+      appBar: AppBar(title: Text(repo.name)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ProfileHeader(owner: repo.owner),
+            const SizedBox(height: 16),
+            DescriptionArea(description: repo.description),
+            Divider(),
+            InfoRow(label: "Forks", value: repo.forksCount.toString()),
+            InfoRow(label: "Created", value: format.format(repo.createdAt)),
+            InfoRow(label: "Last update", value: format.format(repo.updatedAt)),
+            LinkButton(url: Uri.parse(repo.htmlUrl), label: 'Link: $repoUrl'),
+          ],
+        ),
+      ),
+    );
+  }
+}
